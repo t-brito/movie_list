c6d2dca5eaeb76526f5aa634c9e210d0425dc60b
Tiago Brito <tiago.v.brito@gmail.com>
2018-05-28 19:47:49 +0100

    added pyfladesk for standalone desktop app

--- movie_list.py
+++ movie_list.py
@@ -1,4 +1,10 @@
 from app import create_app, cli
 
+from pyfladesk import init_gui
+
 app = create_app()
 cli.register(app)
+
+if __name__ == "__main__":
+    # app.run()
+    init_gui(app, port=8001, width=900, height=600, window_title='Movie List')
